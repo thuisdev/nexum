f17b7a1800979c4e45e72b672d0c5e2d28703a2e
Adrian <adrian.thuis04@gmail.com>
2026-06-02 12:10:59 +0200

    added axios api calls

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -1,5 +1,36 @@
 import axios from "axios"
+import type { LoginCredentials, RegisterCredentials } from "@/types/api"
 
+// Register API Call
+export const loginApi = async (credentials: LoginCredentials) => {
+    const res = await axios.post('/api/auth/login', credentials);
+
+    localStorage.setItem('pactum_token', res.data.token);
+    localStorage.setItem('user', JSON.stringify(res.data.user));
+
+    return res.data;
+};
+
+// Login API Call
+export const registerApi = async (credentials: RegisterCredentials) => {
+    const res = await axios.post('/api/auth/register', credentials);
+
+    localStorage.setItem('pactum_token', res.data.token);
+    localStorage.setItem('user', JSON.stringify(res.data.user));
+
+    return res.data;
+};
+
+// Logout API Call
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+};
+
+
+// Get Me API call
 export const getMe = async () => {
     return axios.get('/api/auth/me')
-}
+};
+
+// Get Token API Call
---
+++ frontend/src/types/api.ts
@@ -0,0 +1,11 @@
+export interface RegisterCredentials {
+    email: string;
+    password: string;
+    role: string;
+    name: string;
+}
+
+export interface LoginCredentials {
+    email: string;
+    password: string;
+}
