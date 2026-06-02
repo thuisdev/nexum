c5805b28c105f4e5a64d299002a6b99002178239
Adrian <adrian.thuis04@gmail.com>
2026-06-02 14:29:47 +0200

    added demo login & register page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,54 @@
-
+import { useAuth } from "@/hooks/useAuth"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
+  const { login } = useAuth()
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+
+    e.preventDefault()
+
+    try {
+      await login(email, password)
+      navigate('/dashboard')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="text"
+              id="email"
+              value={email}
+              onChange={e => { setEmail(e.target.value) }}
+              placeholder=' '
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div>
+            <input type="password"
+              id="password"
+              value={password}
+              onChange={e => { setPassword(e.target.value) }}
+              placeholder=' '
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button type="submit">
+            Sign In
+          </button>
+        </form>
+      </section >
+    </>
   )
 }
 
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,10 +1,48 @@
-
+import { useState } from "react"
+import { useAuth } from "@/hooks/useAuth"
+import { useNavigate } from "react-router-dom"
 
 const RegisterPage = () => {
+  const { register } = useAuth()
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    try {
+      await register({ email, password })
+      navigate('/dashboard')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text"
+            id="email"
+            value={email}
+            onChange={e => { setEmail(e.target.value) }}
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div>
+          <input type="password"
+            id="password"
+            value={password}
+            onChange={e => { setPassword(e.target.value) }}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
   )
 }
 
